775c32d18fcb64cf0fe3540880bbf02b436fda29
unknown <skarolis99@gmail.com>
2019-10-24 20:09:40 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './assets/style.scss'
+import './assets/img/logo@1X.png';
+import './assets/img/Flag icon@1X.png';
+import './assets/img/Crayon icon@1X.png';
+import './assets/img/Rocket icon.png';
+import './assets/img/Gears icon.png';
+import './assets/img/Linkedin icon.png';
+import './assets/img/Mail icon.png';
+import './assets/img/Twitter icon.png';
+import './assets/img/Fb icon.png';
+import './assets/img/1.png';
+import './assets/img/2.png';
+import './assets/img/3.png';
+import './assets/img/4.png';
